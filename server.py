75a9f1446d78259c8c005d97eb7e9d875fd5bb7a
zhckai <141702492+zhckai@users.noreply.github.com>
2024-04-10 20:20:39 +0800

    replication of db upon being locked

--- server.py
+++ server.py
@@ -6,8 +6,10 @@ from hashlib import *
 import random
 import yagmail
 import qrcode
+from initialise_databases import initialise
 from PIL import Image
 #hash data
+db_no = 0
 def calc_sha256_salted(data):
     data = 'LISTEN' + str(data) + '2024'
     if isinstance(data,str):
@@ -20,6 +22,77 @@ def ticket_num(ticket_no):
         return f'0{ticket_no}'
     else:
         return ticket_no
+    
+def remove_duplicate(email):
+    global db_no
+    db = connect(f"/home/yimc/YIJC-LISTEN-2024/Ticketing{db_no}.db")
+    c = db.cursor()
+    c.execute('''SELECT * FROM Bookings WHERE Email = ?''', (email,))
+    bookings = c.fetchall()[1:]
+    tickets = []
+    for book in bookings:
+        ticket = book[1]
+        tickets.append(ticket)
+        c.execute('''DELETE FROM Bookings WHERE TicketNo = ?''', (ticket,))
+        c.execute('''UPDATE Tickets SET Reserved = 0 WHERE TicketNo = ?''', (ticket,))
+    db.commit()
+    db.close()
+
+def poke():
+    global db_no
+    db = connect(f"/home/yimc/YIJC-LISTEN-2024/Ticketing{db_no}.db")
+    c = db.cursor()
+    c.execute('''SELECT Email FROM Bookings''')
+    email_list = c.fetchall()
+
+    email_set = set()
+    email_duplicate = set()
+    for email in email_list:
+        if email not in email_set:
+            email_set.add(email)
+        else:
+            email_duplicate.add(email)
+
+    if email_duplicate:
+        for email in email_duplicate:
+            remove_duplicate(email[0])
+    try:
+        c.execute('''INSERT INTO Bookings(Email, TicketNo, MC_Member, Message, Pin) VALUES(?,?,?,?,?)''', ("zhe_kai@students.edu,sg", "1000", " ", "", "ed3b45b4e6d6ab914d351b3afc3b08169f135d3109777c958a66d2493e537c31"))
+        c.execute('''DELETE FROM Bookings WHERE TicketNo = "1000"''')
+        db.commit()
+        db.close()
+    except Exception as e:
+        print(e)
+        create_new_db()
+        db.commit()
+        db.close()
+
+
+def insert():
+    global db_no
+    db_2 = connect(f"/home/yimc/YIJC-LISTEN-2024/Ticketing{db_no+1}.db")
+    c_2 = db_2.cursor()
+
+
+    db = connect(f"/home/yimc/YIJC-LISTEN-2024/Ticketing{db_no}.db")
+    c = db.cursor()
+
+    c.execute('''SELECT * FROM Bookings''')
+    bookings = c.fetchall()
+    for book in bookings:
+        print(f"BOOK: {book}")
+        c_2.execute('''INSERT INTO Bookings(Email, TicketNo, MC_Member, Message, Pin) VALUES(?,?,?,?,?)''', book)
+        c_2.execute('''UPDATE Tickets SET Reserved = 1 WHERE TicketNo = ?''', (book[1],))
+    db_no += 1
+    db.commit()
+    db.close()
+    db_2.commit()
+    db_2.close()
+    
+def create_new_db():
+    global db_no
+    initialise(db_no + 1)
+    insert()
 # def generate_qr(ticket_no):
     # logo = Image.open("static/Images/logo.png").resize((200, 200), Image.LANCZOS)
     # qr = qrcode.QRCode(
@@ -34,7 +107,7 @@ def ticket_num(ticket_no):
 
 #add all 'MC members' to members
 # file = open('MC Members.txt') #uncomment for offline testing
-file = open('/home/yimc/YIJC-Listen-2024/MC Members.txt') #comment out for offline testing
+file = open('MC Members.txt') #comment out for offline testing
 members = []
 for member in file:
     members.append(member.strip())
@@ -52,8 +125,10 @@ app.secret_key = 'mcyi'
 #----------------------------------------------------------------------------------------------------------------------
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # db = connect('Ticketing.db') #uncomment for offline testing
-    db = connect('/home/yimc/YIJC-Listen-2024/Ticketing.db') #comment out for offline testing
+    poke()
+    global db_no
+    # db = connect('Ticketing{db_no}.db') #uncomment for offline testing
+    db = connect(f'/home/yimc/YIJC-LISTEN-2024/Ticketing{db_no}.db') #comment out for offline testing
     c = db.cursor()
     c.execute('''SELECT * FROM Tickets WHERE Reserved = 0''')
     outOfTickets = c.fetchone() is None
@@ -64,11 +139,13 @@ def index():
 #----------------------------------------------------------------------------------------------------------------------
 @app.route('/validate-email', methods=['GET','POST'])
 def validate():
+    poke()
+    global db_no
     email = request.form.get('email')
     email = email.lower()
 
-    # db = connect('Ticketing.db') #uncomment for offline testing
-    db = connect('/home/yimc/YIJC-Listen-2024/Ticketing.db') #comment out for offline testing
+    # db = connect('Ticketing{db_no}.db') #uncomment for offline testing
+    db = connect(f'/home/yimc/YIJC-LISTEN-2024/Ticketing{db_no}.db') #comment out for offline testing
     c = db.cursor()
     c.execute('''SELECT * FROM AllStudentsEmail
                 WHERE HashedEmail = ?''', (calc_sha256_salted(email),))
@@ -87,8 +164,9 @@ def validate():
             return redirect(url_for('index'))
         else:
             pin =  random.randint(100000, 999999)
-            # db = connect('Ticketing.db') #uncomment for offline testing
-            db = connect('/home/yimc/YIJC-Listen-2024/Ticketing.db') #comment out for offline testing
+            print("Pin is " + str(pin))
+            # db = connect('Ticketing{db_no}.db') #uncomment for offline testing
+            db = connect(f'/home/yimc/YIJC-LISTEN-2024/Ticketing{db_no}.db') #comment out for offline testing
             c = db.cursor()
             print(pin,calc_sha256_salted(email))
             c.execute('''UPDATE AllStudentsEmail SET PIN = ? WHERE HashedEmail = ?''', (pin, calc_sha256_salted(email)))
@@ -109,13 +187,15 @@ def validate():
 #----------------------------------------------------------------------------------------------------------------------
 @app.route('/booking/confirmation', methods=['POST','GET'])
 def confirmation():
+    poke()
+    global db_no
     email = request.form.get('email')
     print(email)
     print(calc_sha256_salted(email))
     pin = request.form.get('pin')
 
-    # db = connect('Ticketing.db') #uncomment for offline testing
-    db = connect('/home/yimc/YIJC-Listen-2024/Ticketing.db') #comment out for offline testing
+    # db = connect('Ticketing{db_no}.db') #uncomment for offline testing
+    db = connect(f'/home/yimc/YIJC-LISTEN-2024/Ticketing{db_no}.db') #comment out for offline testing
     c = db.cursor()
     c.execute('''SELECT PIN FROM AllStudentsEmail WHERE HashedEmail = ?''', (calc_sha256_salted(email),))
 
@@ -134,13 +214,15 @@ def confirmation():
 #----------------------------------------------------------------------------------------------------------------------
 @app.route('/booking/success', methods = ['POST'])
 def success():
+    global db_no
+    poke()
     email = request.form.get('email')
     member = request.form.get('member')
     message = request.form.get('message')
     pin = request.form.get('pin')
     details = [email,member, message]
-    # db = connect("Ticketing.db") #uncomment for offline testing
-    db = connect('/home/yimc/YIJC-Listen-2024/Ticketing.db') #comment out for offline testing
+    # db = connect("Ticketing{db_no}.db") #uncomment for offline testing
+    db = connect(f'/home/yimc/YIJC-LISTEN-2024/Ticketing{db_no}.db') #comment out for offline testing
     c = db.cursor()
     c.execute('''SELECT * from Bookings WHERE Email = ?''', (email,))
     duplicate = c.fetchone()
@@ -152,8 +234,8 @@ def success():
         flash("Please indicate a member to send a shoutout")
         return render_template('confirmation.html', email = email, message = message, pin=pin, members = members, error = '*Please indicate a member to send a shoutout')
     else:
-        # db = connect('Ticketing.db') #uncomment for offline testing
-        db = connect('/home/yimc/YIJC-Listen-2024/Ticketing.db') #comment out for offline testing
+        # db = connect('Ticketing{db_no}.db') #uncomment for offline testing
+        db = connect(f'/home/yimc/YIJC-LISTEN-2024/Ticketing{db_no}.db') #comment out for offline testing
         c = db.cursor()
         c.execute('''SELECT TicketNo FROM Tickets WHERE Reserved = 0''')
         available = c.fetchone()[0]
@@ -184,7 +266,7 @@ def success():
                     4. MC may postpone, cancel or interrupt the event due to dangerous situations or any cause beyond reasonable control.
                     5. As part of security and adherence to college rules, all bags will be checked before entering the venue.
 
-            ''', attachments = [f"/home/yimc/YIJC-Listen-2024/static/QRcodes/ticket_{ticket_num(ticket_no[0])}.png"])
+            ''', attachments = [f"/home/yimc/YIJC-LISTEN-2024/static/QRcodes/ticket_{ticket_num(ticket_no[0])}.png"])
             return render_template('success.html')
         except Exception as error:	
             print(f"error message: {error}")
@@ -193,15 +275,18 @@ def success():
 #----------------------------------------------------------------------------------------------------------------------
 @app.route('/resend-ticket', methods=["GET","POST"])
 def resend_ticket():
+    poke()
     return render_template('resend_ticket.html')
 
 #----------------------------------------------------------------------------------------------------------------------
 @app.route('/resend-ticket/success', methods=["GET","POST"])
 def resend_ticket_success():
+    global db_no
+    poke()
     email = request.form.get('email')
     pin = request.form.get('pin')
-    # db = connect('Ticketing.db') #uncomment for offline testing
-    db = connect('/home/yimc/YIJC-Listen-2024/Ticketing.db') #comment out for offline testing
+    # db = connect('Ticketing{db_no}.db') #uncomment for offline testing
+    db = connect(f'/home/yimc/YIJC-LISTEN-2024/Ticketing{db_no}.db') #comment out for offline testing
     c = db.cursor()
     hashed_email = str(calc_sha256_salted(email))
     c.execute('''SELECT * FROM AllStudentsEmail
@@ -230,7 +315,7 @@ def resend_ticket_success():
                     4. MC may postpone, cancel or interrupt the event due to dangerous situations or any cause beyond reasonable control.
                     5. As part of security and adherence to college rules, all bags will be checked before entering the venue.
 
-                ''', attachments = [f"/home/yimc/YIJC-Listen-2024/static/QRcodes/ticket_{ticket_num(TicketNo[0])}.png"])
+                ''', attachments = [f"/home/yimc/YIJC-LISTEN-2024/static/QRcodes/ticket_{ticket_num(TicketNo[0])}.png"])
                 return render_template("resend_ticket_success.html")
             except Exception as error:
                 print(f"error message: {error}")    
@@ -245,9 +330,11 @@ def resend_ticket_success():
 #----------------------------------------------------------------------------------------------------------------------
 @app.route("/contact")
 def contact():
-	return render_template("contact.html")
+    poke()
+    return render_template("contact.html")
 @app.route("/contact_success", methods = ['GET', 'POST'])
 def contact_success():
+    poke()
     name = request.form.get('Name')
     email = request.form.get("Email")
     text = request.form.get("Text")
